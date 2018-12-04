5a4ce3e5afd7d008eac24b0401d05a9a275ed1e3
b5 <sparkle_pony_2000@qri.io>
2018-12-04 10:52:20 -0500

    feat(InfoStore): intial InfoStore implementation
    We need an interface for storing & caching manifests, and have agreed it's better to start with an
    InfoStore that stores supplimental info, and because dag.Info has a manifest field, we can store manifests here as well

---
+++ store_test.go
@@ -0,0 +1,54 @@
+package dag
+
+import (
+	"context"
+	"testing"
+)
+
+func TestMemInfoStore(t *testing.T) {
+	ctx := context.Background()
+
+	is := NewMemInfoStore()
+	if _, err := is.DAGInfo(ctx, ""); err != ErrInfoNotFound {
+		t.Errorf("expected ErrInfoNotFound for a get to a non-existent ID. got: %s", err)
+	}
+	if deleted, err := is.DeleteDAGInfo(ctx, ""); err != nil {
+		t.Errorf("delete to non-existent key shouldn't error")
+	} else if deleted {
+		t.Errorf("expected empty delete to return false deleted value")
+	}
+
+	g := newGraph([]layer{{8, 2 * kb}})
+	ng := TestingNodeGetter{g}
+
+	info, err := NewInfo(ctx, ng, g[0].Cid())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := is.PutDAGInfo(ctx, info.RootCID().String(), info); err != nil {
+		t.Errorf("error putting info: %s", err.Error())
+	}
+
+	if err := is.PutDAGInfo(ctx, "foo", info); err != nil {
+		t.Errorf("error putting info with arbitrary id key: %s", err.Error())
+	}
+
+	info, err = is.DAGInfo(ctx, info.RootCID().String())
+	if err != nil {
+		t.Errorf("error getting info: %s", err.Error())
+	}
+
+	if deleted, err := is.DeleteDAGInfo(ctx, info.RootCID().String()); err != nil {
+		t.Errorf("error deleting info: %s", err.Error())
+	} else if !deleted {
+		t.Errorf("expected deleted to return true")
+	}
+
+	if deleted, err := is.DeleteDAGInfo(ctx, "foo"); err != nil {
+		t.Errorf("error deleting arbitrary key: %s", err.Error())
+	} else if !deleted {
+		t.Errorf("expected dleted to equal true")
+	}
+
+}
